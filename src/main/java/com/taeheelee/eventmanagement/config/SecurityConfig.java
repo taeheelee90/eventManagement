c43abb7c49f509e2f78954f12a9ef987ea6ed716
taeheelee90 <taehee.lee90@gmail.com>
2020-12-07 17:50:42 +0200

    Add Remeber Me

--- src/main/java/com/taeheelee/eventmanagement/config/SecurityConfig.java
+++ src/main/java/com/taeheelee/eventmanagement/config/SecurityConfig.java
@@ -1,17 +1,29 @@
 package com.taeheelee.eventmanagement.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import com.taeheelee.eventmanagement.Account.AccountService;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private final AccountService accountService;
+	private final DataSource dataSource;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -26,6 +38,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			
 			http.logout()
 				.logoutSuccessUrl("/");
+			
+			/*Remember Me*/
+			http.rememberMe()
+				.userDetailsService(accountService)
+				.tokenRepository(tokenRepository());
+	}
+	
+	@Bean
+	public PersistentTokenRepository tokenRepository() {
+		JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+		jdbcTokenRepository.setDataSource(dataSource);
+		return jdbcTokenRepository;
+		
 	}
 	
 	// Static resources are allowed with authentication
---
+++ src/test/java/com/taeheelee/eventmanagement/main/MainControllerTest.java
@@ -0,0 +1,99 @@
+package com.taeheelee.eventmanagement.main;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.taeheelee.eventmanagement.Account.AccountRepository;
+import com.taeheelee.eventmanagement.Account.AccountService;
+import com.taeheelee.eventmanagement.Account.SignUpForm;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MainControllerTest {
+	
+	@Autowired MockMvc mockMvc;
+	@Autowired AccountService accountService;
+	@Autowired AccountRepository accountRepository;
+		
+	@BeforeEach
+	void beforeEach() {
+		SignUpForm signUpForm = new SignUpForm();
+		signUpForm.setNickname("testuser");
+		signUpForm.setEmail("test@email.com");
+		signUpForm.setPassword("12345678");
+		accountService.processNewAccount(signUpForm);
+	}
+	
+	@AfterEach
+	void afterEach() {
+		accountRepository.deleteAll();
+	}
+	
+	
+	
+	@DisplayName("Email Login Success")
+	@Test
+	void login_with_email() throws Exception {		
+				
+		mockMvc.perform(post("/login")
+				.param("username", "test@email.com")
+				.param("password", "12345678")
+				.with(csrf()))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/"))
+				.andExpect(authenticated().withUsername("testuser"));;
+
+	}
+	
+	
+	@DisplayName("Nickname Login Success")
+	@Test
+	void login_with_nickname() throws Exception {
+		mockMvc.perform(post("/login")
+				.param("username", "testuser")
+				.param("password", "12345678")
+				.with(csrf()))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/"))
+				.andExpect(authenticated().withUsername("testuser"));;
+
+	}
+	
+	@DisplayName("Login Fail")
+	@Test
+	void login_fail() throws Exception {
+		mockMvc.perform(post("/login")
+				.param("username", "wronguser")
+				.param("password", "1111111")
+				.with(csrf()))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/login?error"))
+				.andExpect(unauthenticated());;
+
+	}
+	
+	@WithMockUser	
+	@DisplayName("Logout")
+	@Test
+	void logout() throws Exception {
+		mockMvc.perform(post("/logout")
+				.with(csrf()))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/"))
+				.andExpect(unauthenticated());;
+
+	}
+}
