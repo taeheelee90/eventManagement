bcb1819326c094ce8db2f4c1dcd57166b60a33e8
taeheelee90 <taehee.lee90@gmail.com>
2020-12-07 12:41:09 +0200

    Add login and logout

--- src/main/java/com/taeheelee/eventmanagement/config/SecurityConfig.java
+++ src/main/java/com/taeheelee/eventmanagement/config/SecurityConfig.java
@@ -20,6 +20,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 					"/email-login", "check-email-login", "/login-link").permitAll()
 			.mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
 			.anyRequest().authenticated();
+			
+			http.formLogin()
+				.loginPage("/login").permitAll();
+			
+			http.logout()
+				.logoutSuccessUrl("/");
 	}
 	
 	// Static resources are allowed with authentication
