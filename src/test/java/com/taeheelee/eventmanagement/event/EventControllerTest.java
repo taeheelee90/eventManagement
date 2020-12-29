f0be305b9784921544853c7b7ff44a93cfcee172
taeheelee90 <taehee.lee90@gmail.com>
2020-12-29 11:44:46 +0200

    EventController Test

--- src/test/java/com/taeheelee/eventmanagement/event/EventControllerTest.java
+++ src/test/java/com/taeheelee/eventmanagement/event/EventControllerTest.java
@@ -1,6 +1,8 @@
 package com.taeheelee.eventmanagement.event;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -31,64 +33,54 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class EventControllerTest {
 
-	@Autowired MockMvc mockMvc;
-	@Autowired EventService eventService;
-	@Autowired EventRepository eventRepository;
-	@Autowired AccountRepository accountRepository;
-	
+	@Autowired
+	protected MockMvc mockMvc;
+	@Autowired
+	protected EventService eventService;
+	@Autowired
+	protected EventRepository eventRepository;
+	@Autowired
+	protected AccountRepository accountRepository;
+
 	@AfterEach
 	void afterEach() {
 		accountRepository.deleteAll();
 	}
-	
+
 	@Test
 	@WithAccount("testUser")
 	@DisplayName("View Event Form")
 	void showEventForm() throws Exception {
-		mockMvc.perform(get("/new-event"))
-				.andExpect(status().isOk())
-				.andExpect(view().name("event/form"))
-				.andExpect(model().attributeExists("account"))
-				.andExpect(model().attributeExists("eventForm"));
-				
+		mockMvc.perform(get("/new-event")).andExpect(status().isOk()).andExpect(view().name("event/form"))
+				.andExpect(model().attributeExists("account")).andExpect(model().attributeExists("eventForm"));
+
 	}
-	
+
 	@Test
 	@WithAccount("testUser")
 	@DisplayName("Create Event - success")
-	void createEvent_success() throws Exception{
-		mockMvc.perform(post("/new-event")
-				.param("path", "test-path")
-				.param("title", "test-title")
+	void createEvent_success() throws Exception {
+		mockMvc.perform(post("/new-event").param("path", "test-path").param("title", "test-title")
 				.param("shortDescription", "test-short-description")
-				.param("fullDescription", "test-full-description-for-test-event")
-				.with(csrf()))
-				.andExpect(status().is3xxRedirection())
-				.andExpect(redirectedUrl("/event/test-path"));		
-	
+				.param("fullDescription", "test-full-description-for-test-event").with(csrf()))
+				.andExpect(status().is3xxRedirection()).andExpect(redirectedUrl("/event/test-path"));
+
 	}
-	
+
 	@Test
 	@WithAccount("testUser")
 	@DisplayName("Create Event - fail")
-	void createEvent_fail() throws Exception{
-		mockMvc.perform(post("/new-event")
-				.param("path", " ")
-                .param("title", "test-title")
-                .param("shortDescription", "test-short-description")
-                .param("fullDescription", "test-full-description-for-test-event")
-                .with(csrf()))
-                .andExpect(status().isOk())
-                .andExpect(view().name("event/form"))
-                .andExpect(model().hasErrors())
-                .andExpect(model().attributeExists("eventForm"))
-                .andExpect(model().attributeExists("account"));
-		
+	void createEvent_fail() throws Exception {
+		mockMvc.perform(post("/new-event").param("path", " ").param("title", "test-title")
+				.param("shortDescription", "test-short-description")
+				.param("fullDescription", "test-full-description-for-test-event").with(csrf()))
+				.andExpect(status().isOk()).andExpect(view().name("event/form")).andExpect(model().hasErrors())
+				.andExpect(model().attributeExists("eventForm")).andExpect(model().attributeExists("account"));
+
 		Event event = eventRepository.findByPath("test-path");
 		assertNull(event);
 	}
-	
-	
+
 	@Test
 	@WithAccount("testUser")
 	@DisplayName("View Event Information")
@@ -98,14 +90,58 @@ public class EventControllerTest {
 		event.setTitle("test-title");
 		event.setShortDescription("test-short-description");
 		event.setFullDescription("test-full-description-for-test-event");
-		
+
 		Account testUser = accountRepository.findByNickname("testUser");
 		eventService.createNewEvent(event, testUser);
-		
-		mockMvc.perform(get("/event/test-path"))
-			   .andExpect(view().name("event/view"))
-			   .andExpect(model().attributeExists("account"))
-			   .andExpect(model().attributeExists("event"));
-			
+
+		mockMvc.perform(get("/event/test-path")).andExpect(view().name("event/view"))
+				.andExpect(model().attributeExists("account")).andExpect(model().attributeExists("event"));
+
+	}
+
+	@Test
+	@WithAccount("testUser")
+	@DisplayName("Join Event")
+	void joinEvent() throws Exception {
+		Account manager = createAccount("manager");
+
+		Event event = createEvent("test-event", manager);
+
+		mockMvc.perform(get("/event/" + event.getPath() + "/join")).andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/event/" + event.getPath() + "/members"));
+
+		Account testUser = accountRepository.findByNickname("testUser");
+		assertTrue(event.getMembers().contains(testUser));
+	}
+
+	@Test
+	@WithAccount("testUser")
+	@DisplayName("Leave Event")
+	void leaveEvent() throws Exception {
+		Account manager = createAccount("manager");
+		Event event = createEvent("test-event", manager);
+
+		Account testUser = accountRepository.findByNickname("testUser");
+		eventService.addMember(event, testUser);
+
+		mockMvc.perform(get("/event/" + event.getPath() + "/leave")).andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/event/" + event.getPath() + "/members"));
+
+		assertFalse(event.getMembers().contains(testUser));
+	}
+
+	protected Account createAccount(String nickname) {
+		Account newAccount = new Account();
+		newAccount.setNickname(nickname);
+		newAccount.setEmail(nickname + "@email.com");
+		accountRepository.save(newAccount);
+		return newAccount;
+	}
+
+	protected Event createEvent(String path, Account manager) {
+		Event event = new Event();
+		event.setPath(path);
+		eventService.createNewEvent(event, manager);
+		return event;
 	}
 }
---
+++ src/test/java/com/taeheelee/eventmanagement/event/EventSettingsControllerTest.java
@@ -0,0 +1,77 @@
+package com.taeheelee.eventmanagement.event;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.taeheelee.eventmanagement.WithAccount;
+import com.taeheelee.eventmanagement.domain.Account;
+import com.taeheelee.eventmanagement.domain.Event;
+
+import lombok.RequiredArgsConstructor;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
+public class EventSettingsControllerTest extends EventControllerTest {
+
+	@Test
+	@WithAccount("testUser")
+	@DisplayName("Show description edit form - Fail")
+	void updateDescriptionForm_fail() throws Exception {
+		Account account = createAccount("account");
+		Event event = createEvent("test-event", account);
+
+		mockMvc.perform(get("/event/" + event.getPath() + "/settings/description")).andExpect(status().isForbidden());
+	}
+
+	@Test
+	@WithAccount("testUser")
+	@DisplayName("Show description edit form - success")
+	void updateDescriptionForm_success() throws Exception {
+		Account manager = accountRepository.findByNickname("testUser");
+		Event event = createEvent("test-event", manager);
+
+		mockMvc.perform(get("/event/" + event.getPath() + "/settings/description")).andExpect(status().isOk())
+				.andExpect(view().name("event/settings/description"))
+				.andExpect(model().attributeExists("eventDescriptionForm"))
+				.andExpect(model().attributeExists("account")).andExpect(model().attributeExists("event"));
+	}
+
+	@Test
+	@WithAccount("testUser")
+	@DisplayName("Update description - success")
+	void upadateDescription_success() throws Exception {
+		Account manager = accountRepository.findByNickname("testUser");
+		Event event = createEvent("test-event", manager);
+
+		String url = "/event/" + event.getPath() + "/settings/description";
+		mockMvc.perform(post(url).param("shortDescription", "short description")
+				.param("fullDescription", "full description").with(csrf())).andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl(url)).andExpect(flash().attributeExists("message"));
+
+	}
+
+	@Test
+	@WithAccount("testUser")
+	@DisplayName("Update description - fail")
+	void upadateDescription_fail() throws Exception {
+		Account manager = accountRepository.findByNickname("testUser");
+		Event event = createEvent("test-event", manager);
+
+		String url = "/event/" + event.getPath() + "/settings/description";
+		mockMvc.perform(
+				post(url).param("shortDescription", "").param("fullDescription", "full description").with(csrf()))
+				.andExpect(status().isOk()).andExpect(model().hasErrors())
+				.andExpect(model().attributeExists("eventDescriptionForm")).andExpect(model().attributeExists("event"))
+				.andExpect(model().attributeExists("account"));
+	}
+
+}
