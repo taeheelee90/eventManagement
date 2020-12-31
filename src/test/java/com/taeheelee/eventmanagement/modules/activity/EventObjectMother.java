f6dc15be4f4bfcae449ca68ab6bb855e5afc516d
taeheelee90 <taehee.lee90@gmail.com>
2020-12-31 10:58:46 +0200

    Add event test

---
+++ src/test/java/com/taeheelee/eventmanagement/modules/activity/EventObjectMother.java
@@ -0,0 +1,27 @@
+package com.taeheelee.eventmanagement.modules.activity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.taeheelee.eventmanagement.modules.account.Account;
+import com.taeheelee.eventmanagement.modules.event.Event;
+import com.taeheelee.eventmanagement.modules.event.EventRepository;
+import com.taeheelee.eventmanagement.modules.event.EventService;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class EventObjectMother {
+
+	@Autowired EventService eventService;
+	@Autowired EventRepository eventRepository;
+	
+	public Event createEvent(String path, Account manager) {
+		Event event  = new Event();
+		event.setPath(path);
+		eventService.createNewEvent(event, manager);
+		return event;
+		
+	}
+}
---
+++ src/test/java/com/taeheelee/eventmanagement/modules/activity/EventTest.java
@@ -0,0 +1,85 @@
+package com.taeheelee.eventmanagement.modules.activity;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.taeheelee.eventmanagement.modules.account.Account;
+import com.taeheelee.eventmanagement.modules.account.UserAccount;
+import com.taeheelee.eventmanagement.modules.event.Event;
+
+class EventTest {
+
+	Event event;
+	Account account;
+	UserAccount userAccount;
+
+	@BeforeEach
+	void beforeEach() {
+		event = new Event();
+		account = new Account();
+		account.setNickname("testUser");
+		account.setPassword("12345678");
+		userAccount = new UserAccount(account);
+	}
+
+	@DisplayName("Published, Recuriting, Not a member nor manager - can join")
+	@Test
+	void canJoin() {
+		event.setPublished(true);
+		event.setRegistration(true);
+
+		assertTrue(event.isJoinable(userAccount));
+	}
+
+	@DisplayName("Published, Recruiting, Manager - cannot join")
+	@Test
+	void cannotJoin_manager() {
+		event.setPublished(true);
+		event.setRegistration(true);
+		event.addManager(account);
+
+		assertFalse(event.isJoinable(userAccount));
+	}
+
+	@DisplayName("Published, Recruting, Member - cannot join again")
+	@Test
+	void cannotJoin_member() {
+		event.setPublished(true);
+		event.setRegistration(true);
+		event.addMember(account);
+
+		assertFalse(event.isJoinable(userAccount));
+	}
+
+	@DisplayName("Not published, Not recruting - cannot join")
+	@Test
+	void cannotJoin_not_published_not_recruting() {
+		event.setPublished(false);
+		event.setRegistration(false);
+
+		assertFalse(event.isJoinable(userAccount));
+
+		event.setPublished(false);
+		event.setRegistration(true);
+
+		assertFalse(event.isJoinable(userAccount));
+	}
+
+	@DisplayName("Check isManager")
+	@Test
+	void isManager() {
+		event.addManager(account);
+		assertTrue(event.isManager(userAccount));
+	}
+
+	@DisplayName("Check isMember")
+	@Test
+	void isMember() {
+		event.addMember(account);
+		assertTrue(event.isMember(userAccount));
+	}
+}
