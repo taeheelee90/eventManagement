ccf6fe7de51f020eb5d8af895d3cb7c8028e45ba
taeheelee90 <taehee.lee90@gmail.com>
2021-01-01 20:05:57 +0200

    Show unread notification to authenticated user

---
+++ src/main/java/com/taeheelee/eventmanagement/modules/notification/NotificationInterceptor.java
@@ -0,0 +1,37 @@
+package com.taeheelee.eventmanagement.modules.notification;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.taeheelee.eventmanagement.modules.account.Account;
+import com.taeheelee.eventmanagement.modules.account.UserAccount;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationInterceptor implements HandlerInterceptor {
+	
+	private final NotificationRepository notificationRepository;
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if(modelAndView != null && !isRedirectView(modelAndView) && authentication != null && authentication.getPrincipal() instanceof UserAccount) {
+			Account account = ((UserAccount)authentication.getPrincipal()).getAccount();
+			long count = notificationRepository.countByAccountAndChecked(account, false);
+			modelAndView.addObject("hasNotification", count > 0);
+		}
+	}
+
+	private boolean isRedirectView(ModelAndView modelAndView) {
+		
+		return modelAndView.getViewName().startsWith("redirect:");
+	}
+}
--- src/main/java/com/taeheelee/eventmanagement/modules/notification/NotificationRepository.java
+++ src/main/java/com/taeheelee/eventmanagement/modules/notification/NotificationRepository.java
@@ -3,7 +3,11 @@ package com.taeheelee.eventmanagement.modules.notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.taeheelee.eventmanagement.modules.account.Account;
+
 @Transactional(readOnly = true)
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+	long countByAccountAndChecked(Account account, boolean checked);
 
 }
